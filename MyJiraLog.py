c27ba4511bebe1ee682e40c3113796a8af8287bb
Ben Staniford <bstaniford@beyondtrust.com>
2024-09-27 14:16:58 +0100

    Filter out old data

--- MyJiraLog.py
+++ MyJiraLog.py
@@ -1,13 +1,24 @@
 import os
 import tempfile
 import subprocess
+import datetime
 
 class MyJiraLog:
     def __init__(self):
         self.server = os.getenv('LOGSERVER')
         self.user = os.getenv('LOGUSER')
 
-    def get_log(self):
+    def parse_date(self, line):
+        try:
+            date_time = line.split(',')[0]
+            date = date_time.split(' ')[0]
+            day, month, year = date.split('/')
+            date_of_line = datetime.datetime(int(year), int(month), int(day))
+            return date_of_line
+        except:
+            return None
+
+    def get_log(self, days_old = 14):
         self.tempfile = tempfile.NamedTemporaryFile(delete=False)
         log_path = f'{self.user}@{self.server}:~/sprint-snapshot.log'
         process = subprocess.Popen(['scp', log_path, self.tempfile.name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -16,6 +27,23 @@ class MyJiraLog:
         if os.stat(self.tempfile.name).st_size == 0:
             os.unlink(self.tempfile.name)
             raise Exception('Log file is empty, failed to retrieve log file from server.')
+
+        # Calculate today - days_old
+        today = datetime.datetime.now()
+        n_days_ago = datetime.timedelta(days=days_old)
+        too_old = today - n_days_ago
+
+        # Filter out lines older than n_days_ago
+        filtered_lines = []
+        with open(self.tempfile.name, 'r') as f:
+            lines = f.readlines()
+            for i, line in enumerate(lines):
+                date_of_line = self.parse_date(line)
+                if date_of_line is None or date_of_line > too_old:
+                    filtered_lines.append(line)
+
+        with open(self.tempfile.name, 'w') as f:
+            f.writelines(filtered_lines)
 
         return self.tempfile.name
 
