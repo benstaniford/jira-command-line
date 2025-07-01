4e455dd6090e5a539c957d237b728a5de3bc88f5
Ben Staniford <ben.staniford@gmail.com>
2025-07-01 18:53:39 +0100

    Type checking

--- commands/base_command.py
+++ commands/base_command.py
@@ -1,25 +1,35 @@
 from abc import ABC, abstractmethod
+from typing import Any, Optional
 
 class BaseCommand(ABC):
     """Base class for all jira commands"""
     
     @property
     @abstractmethod
-    def shortcut(self):
+    def shortcut(self) -> str:
         """The keyboard shortcut for this command"""
         pass
     
     @property
     @abstractmethod
-    def description(self):
+    def description(self) -> str:
         """Description of what this command does"""
         pass
     
     @abstractmethod
-    def execute(self, ui, view, jira, mygit=None, mygithub=None, config=None, **kwargs):
+    def execute(
+        self,
+        ui: Any,
+        view: Any,
+        jira: Any,
+        mygit: Optional[Any] = None,
+        mygithub: Optional[Any] = None,
+        config: Optional[Any] = None,
+        **kwargs: Any
+    ) -> Any:
         """Execute the command"""
         pass
     
-    def get_help_text(self):
+    def get_help_text(self) -> str:
         """Get help text for this command"""
         return f"{self.shortcut}: {self.description}"
