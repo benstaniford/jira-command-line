5d61c955382247c73b28a5400d93fce0e0fdaae1
Tamas Varady <tvarady@beyondtrust.com>
2025-08-21 14:11:07 +0100

    Add double spaces as an allowed step part rpefix

--- libs/JiraXrayIssue.py
+++ libs/JiraXrayIssue.py
@@ -205,7 +205,7 @@ Then <Step 3>
                         elif line_lwr_j.startswith('steps:'):
                             for k in range(j + 1, len(lines)):
                                 line_lwr_k = lines[k].lower().strip()
-                                if line_lwr_k.startswith(('given', 'and', 'when', 'then', 'but', '|', 'example')):
+                                if line_lwr_k.startswith(('given', 'and', 'when', 'then', 'but', '|', 'example', '  ')):
                                     steps.append(lines[k].strip())
                                 else:
                                     break
